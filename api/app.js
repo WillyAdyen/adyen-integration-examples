75bfc3bd41b8596049b31e41df369eebce3ac73b
Willy Larsson <willy.larsson@live.se>
2021-07-15 16:16:57 +0200

    Heroku is fun

--- api/app.js
+++ api/app.js
@@ -52,6 +52,12 @@ app.use(function(err, req, res, next) {
   });
 });
 
+app.use(bp.json());
+
+app.post("/hook", (req, res) => {
+  console.log(req.body); // Call your action on the request here
+  res.status(200).end(); // Responding is important
+})
 
 const peer = express()
 peer.use(bp.json());
